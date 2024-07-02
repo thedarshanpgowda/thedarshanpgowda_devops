69162e9ca9d7098f93e2a249d1e5c27bf63d1acb
Guru227 <narain2000@gmail.com>
2024-07-02 09:30:03 +0530

    change arguments

--- Guru/main.py
+++ Guru/main.py
@@ -6,8 +6,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 1
-    b = 2
+    a = 5
+    b = 6
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
