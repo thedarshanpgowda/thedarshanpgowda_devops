5668a318e0ccf8583530b90e3e6696c2556cac8c
Gurusankar K <44615409+Guru227@users.noreply.github.com>
2024-07-02 15:51:12 +0530

    mul_1 (#54)

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 1
-    b = 2
+    a = 3
+    b = 4
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
