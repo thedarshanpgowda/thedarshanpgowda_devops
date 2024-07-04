82fc7d16f85fa032fe4932e3fed586cbc5826c25
Sakshibraj <142163644+sakshibalaraj@users.noreply.github.com>
2024-07-05 01:34:58 +0530

    new pulll request (#51)
    * new commit - Sakshii
    
    * mul_1
    
    * div_1
    
    ---------
    
    Co-authored-by: Gurusankar K <44615409+Guru227@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 15
-    b = 8
+    a = 20
+    b = 30
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
