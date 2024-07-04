7c089149d2027fbe671c58a8a3feb460882d79cd
vasista <148851443+SanjayVasista@users.noreply.github.com>
2024-07-05 01:39:42 +0530

    sanjay (#69)
    * initial commit
    
    * new
    
    * new
    
    ---------
    
    Co-authored-by: Gurusankar K <44615409+Guru227@users.noreply.github.com>

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def main():
     a = 10
     b = 50
     
-    print(f"Addition of {a} and {b}: {add(a, b)}")#this is from main1
+    print(f"Addition of {a} and {b}: {add(a, b)}") #this is from main1
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
     print(f"Multiplication of {a} and {b}: {multiply(a, b)}")
     print(f"Division of {a} and {b}: {divide(a, b)}")
