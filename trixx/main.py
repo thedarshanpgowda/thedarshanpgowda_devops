d9e54b73046c70c199f0f6e7a680c6841c0b71fe
Trishal-Ai <138599718+Trishal-Ai@users.noreply.github.com>
2024-07-05 01:27:07 +0530

    this is my commit (#25)
    * this is my commit
    
    * new
    
    ---------
    
    Co-authored-by: Gurusankar K <44615409+Guru227@users.noreply.github.com>

--- trixx/main.py
+++ trixx/main.py
@@ -6,8 +6,8 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 20
-    b = 10
+    a = 10
+    b = 5
     
     print(f"Addition of {a} and {b}: {add(a, b)}")
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
