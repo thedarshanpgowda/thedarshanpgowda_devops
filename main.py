4743d0e61ff3c1c68f0f4c9ef073ebda54863cad
vasista <148851443+SanjayVasista@users.noreply.github.com>
2024-07-05 01:37:20 +0530

    Main1 (#63)
    * initial commit
    
    * new
    
    * new
    
    ---------
    
    Co-authored-by: Gurusankar K <44615409+Guru227@users.noreply.github.com>

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ from division import divide
 def main():
     print("Simple Calculator Program")
     
-    a = 20
-    b = 30
+    a = 10
+    b = 50
     
-    print(f"Addition of {a} and {b}: {add(a, b)}")
+    print(f"Addition of {a} and {b}: {add(a, b)}")#this is from main1
     print(f"Subtraction of {a} and {b}: {subtract(a, b)}")
     print(f"Multiplication of {a} and {b}: {multiply(a, b)}")
     print(f"Division of {a} and {b}: {divide(a, b)}")
